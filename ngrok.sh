37efa063967f944b2c72e19a03d1ac9419cdc3cc
znzazy <112530266+znzazy@users.noreply.github.com>
2022-09-15 04:53:50 +0200

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -8,6 +8,6 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/a/8exBtGpBr59/ngrok-2.3.40-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 
