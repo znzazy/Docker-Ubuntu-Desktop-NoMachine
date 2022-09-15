9b3abb68e8198eb838489ea91457015028d871f0
znzazy <112530266+znzazy@users.noreply.github.com>
2022-09-15 04:56:28 +0200

    Update nomachine-mate.sh

--- nomachine-mate.sh
+++ nomachine-mate.sh
@@ -1,4 +1,4 @@
-wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://github.com/znzazy/Docker-Ubuntu-Desktop-NoMachine/blob/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 
